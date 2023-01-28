1157027164afaac98391c22d279f4203a39183c6
Alejandro Quiñones Caicedo <50637445+Legnatbird@users.noreply.github.com>
2023-01-28 14:16:14 -0500

    Fix theme selection Now you can change around light, dark or system

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,3 +1,4 @@
+import { createCookieStorageManager } from '@chakra-ui/color-mode';
 import { ChakraProvider } from '@chakra-ui/react';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import LogsProvider from './contexts/logging/logs-provider';
@@ -21,11 +22,15 @@ import './styles/scroll.css';
 import Logs from './screens/logs';
 
 export default function App() {
+  const manager = createCookieStorageManager(
+    'color-mode',
+    `${theme.config.initialColorMode}`
+  );
   return (
     <LogsProvider>
       <InstancesProvider>
         <VersionsProvider>
-          <ChakraProvider theme={theme}>
+          <ChakraProvider theme={theme} colorModeManager={manager}>
             <Router>
               <TitleBar title="OpenMC" />
               <Navbar />
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,14 +1,13 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import theme from './theme';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 root.render(
   <>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <ColorModeScript storageKey="color-mode" />
     <App />
   </>
 );
