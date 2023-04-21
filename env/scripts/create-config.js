bc9a471b1b289edd4f49c49e4c62a5d455b1dfce
Komrade FMX <62594965+KomradeFMX@users.noreply.github.com>
2023-04-21 11:57:56 +0200

    feature: config page
    Added a config page with username and ram adjustment

---
+++ env/scripts/create-config.js
@@ -0,0 +1,28 @@
+const jsonfile = require('jsonfile');
+const path = require('path');
+
+function getAppdataDir() {
+  return (
+    process.env.APPDATA ||
+    (process.platform === 'darwin'
+      ? `${process.env.HOME}/Library/Preferences`
+      : `${process.env.HOME}/.local/share`)
+  );
+}
+
+function getLauncherDir() {
+  return path.join(getAppdataDir(), '.openmc');
+}
+
+console.log(getLauncherDir());
+
+const config = {
+  username: 'Player',
+  mode: 'offline',
+  minRam: 1024,
+  maxRam: 2048,
+};
+
+const configDir = path.join(getLauncherDir(), 'config.json');
+
+jsonfile.writeFileSync(configDir, config);
